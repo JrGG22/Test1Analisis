5eb518198dd3432bb831f6783e47abc89920b8f0
JrGG22 <nicolay034@gmail.com>
2024-09-19 13:28:45 -0500

    Calculadora Parte 2 (Multiplicacion y divicion)

--- Division.java
+++ Division.java
@@ -1,3 +1,19 @@
-public class Division {
+import java.util.Scanner;
 
+public class Division {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Primer numero para Dividir:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Segundo numero para Dividir:");
+        int num2 = scanner.nextInt();
+
+        int resultado = num1 / num2;
+
+        System.out.println("La restar de " + num1 + " y " + num2 + " es: " + resultado);
+
+        scanner.close();
+    }
 }
--- Multiplicacion.java
+++ Multiplicacion.java
@@ -1,3 +1,17 @@
 public class Multiplicacion {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Primer numero para Multiplicar:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Segundo numero para Multiplicar:");
+        int num2 = scanner.nextInt();
+
+        int resultado = num1 * num2;
+
+        System.out.println("La restar de " + num1 + " y " + num2 + " es: " + resultado);
+
+        scanner.close();
+    }
 }
