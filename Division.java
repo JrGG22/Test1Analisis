0e36121be5cfcf255c7e798af1c978b69f55ea9d
JrGG22 <nicolay034@gmail.com>
2024-09-20 16:56:08 -0500

    CalculadoraVersion3

--- Division.java
+++ Division.java
@@ -1,19 +1,14 @@
+package Prueba;
 import java.util.Scanner;
-
 public class Division {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Primer numero para Dividir:");
+        System.out.println("Primer numero para restar:");
         int num1 = scanner.nextInt();
-
-        System.out.println("Segundo numero para Dividir:");
+        System.out.println("Segundo numero para restar:");
         int num2 = scanner.nextInt();
-
         int resultado = num1 / num2;
-
         System.out.println("La restar de " + num1 + " y " + num2 + " es: " + resultado);
-
         scanner.close();
     }
 }
