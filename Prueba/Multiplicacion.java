7597dd9b9e68594ff387dfaecaf1d37fee7671a0
JrGG22 <nicolay034@gmail.com>
2024-09-26 12:35:24 -0500

    CambioOrtografia 2

--- Prueba/Multiplicacion.java
+++ Prueba/Multiplicacion.java
@@ -8,7 +8,7 @@ public class Multiplicacion {
         System.out.println("Segundo numero para Multiplicar:");
         int num2 = scanner.nextInt();
         int resultado = num1 * num2;
-        System.out.println("La restar de " + num1 + " y " + num2 + " es: " + resultado);
+        System.out.println("La multiplicacion de " + num1 + " y " + num2 + " es: " + resultado);
         scanner.close();
     }
 }
