d0564c2e27929a78591e25266fb8b299370abc7d
JrGG22 <nicolay034@gmail.com>
2024-09-26 12:14:05 -0500

    CambiOrtigrafia

--- Prueba/Modulo.java
+++ Prueba/Modulo.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Modulo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Primer numero para restar:");
+        System.out.println("Primer numero para hacer modulo:");
         int num1 = scanner.nextInt();
-        System.out.println("Segundo numero para restar:");
+        System.out.println("Segundo numero para hacer modulo:");
         int num2 = scanner.nextInt();
         int resultado = num1 - num2;
         System.out.println("La restar de " + num1 + " y " + num2 + " es: " + resultado);
